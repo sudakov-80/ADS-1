c9fd495c9c166d6f524077dcd7cecbfc535b873c
sudakov-80 <32332472+sudakov-80@users.noreply.github.com>
2022-02-07 11:41:33 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -2,19 +2,22 @@
 #include <cstdint>
 #include "alg.h"
 
-
 bool checkPrime(uint64_t value) {
-// вставьте код функции
+  // вставьте код функции
+  return true;
 }
 
 uint64_t nPrime(uint64_t n) {
-// вставьте код функции
+  // вставьте код функции
+  return 1;
 }
 
 uint64_t nextPrime(uint64_t value) {
-// вставьте код функции
+  // вставьте код функции
+  return 1;
 }
 
 uint64_t sumPrime(uint64_t hbound) {
-// вставьте код функции
+  // вставьте код функции
+  retuen 1;
 }
